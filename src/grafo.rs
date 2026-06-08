f3bb1bb20e5271935b3829be805fffcc502513e4
Diego Francisco Santos Morales <sm24012@ues.edu.sv>
2026-06-07 22:08:18 -0600

    Compañero 2(SM24012): agrega archivo grafo.rs y definicion de UnGraph

--- src/grafo.rs
+++ src/grafo.rs
@@ -1,6 +1,16 @@
+use petgraph::graph::UnGraph;
 
+// Definición técnica del Grafo para el equipo
+pub type RedSocial = UnGraph<&'static str, u32>;
 
+/// Inicializa el grafo vacío. Se eligió UnGraph porque la amistad es mutua.
+pub fn crear_grafo() -> RedSocial {
+    println!("Iniciando infraestructura de la Red Social..."); 
+    // Usamos default() o new() para inicializar el grafo correctamente
+    UnGraph::default()
+}
 
-// grafo.rs
-// Autor: Compañero 2 - [Diego Santos]
-// Descripción: Clase grafo para red social de amigos
+/// Función auxiliar para que el Compañero 5 pueda imprimir el grafo fácilmente
+pub fn obtener_descripcion_tecnica() -> &'static str {
+    "Tipo de Grafo: No dirigido (UnGraph) | Librería: Petgraph"
+}
